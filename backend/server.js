dfcb8d745b1740b8b9d526d09bb17a63239ca59c
purnale <bhaktipurnale89@gmail.com>
2024-03-11 10:36:43 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path")
 const connectDb = require("./config/connectDb");
 const app = express();
 const cors = require('cors');
 
 const dotenv = require("dotenv").config();
+
+// const __dirname = path.resolve();
 
 // custom port
 const PORT = process.env.PORT || 3001;
@@ -17,6 +20,13 @@ app.use(express.json());
 
 // API routes
 app.use("/api/flowers", require("./routes/flowers.routes"));
+
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
